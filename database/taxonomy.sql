0ab5bf7655b52884dd88b6f3239d093efe65ad6e
Laurence Finston <Laurence.Finston@gmx.de>
2021-08-09 19:16:22 +0200

    Initial version.

---
+++ database/taxonomy.sql
@@ -0,0 +1,61 @@
+
+https://en.wikipedia.org/wiki/Mammal
+https://en.wikipedia.org/wiki/Clade
+https://en.wikipedia.org/wiki/Taxonomy_(biology)
+
+
+Kingdom: 	Animalia
+Phylum: 	Chordata
+Superclass: 	Tetrapoda
+Clade: 	Reptiliomorpha
+Clade: 	Amniota
+Clade: 	Synapsida
+Clade: 	Mammaliaformes
+Class: 	Mammalia
+Linnaeus, 1758
+Living subgroups
+List
+
+        Subclass Yinotheria
+            Infraclass Australosphenida
+                Order Monotremata
+        Subclass Theriiformes
+            Infraclass Holotheria
+                Superlegion Trechnotheria
+                    Legion Cladotheria
+                        Supercohort Theria
+                            Cohort Marsupialia
+                                Superorder Ameridelphia
+                                    Order Didelphimorphia
+                                    Order Paucituberculata
+                                Superorder Australidelphia
+                                    Order Microbiotheria
+                                    Order Dasyuromorphia
+                                    Order Notoryctemorphia
+                                    Order Peramelemorphia
+                                    Order Diprotodontia
+                            Cohort Placentalia
+                                Superorder Xenarthra
+                                    Order Cingulata
+                                    Order Pilosa
+                                Superorder Afroinsectiphilia
+                                    Order Afrosoricida
+                                    Order Macroscelidea
+                                    Order Tubulidentata
+                                Superorder Paenungulata
+                                    Order Hyracoidea
+                                    Order Sirenia
+                                    Order Proboscidea
+                                Superorder Euarchontoglires
+                                    Order Scandentia
+                                    Order Lagomorpha
+                                    Order Rodentia
+                                    Order Dermoptera
+                                    Order Primates
+                                Superorder Laurasiatheria
+                                    Order Eulipotyphla
+                                    Order Chiroptera
+                                    Order Cetartiodactyla
+                                    Order Perissodactyla
+                                    Order Pholidota
+                                    Order Carnivora
